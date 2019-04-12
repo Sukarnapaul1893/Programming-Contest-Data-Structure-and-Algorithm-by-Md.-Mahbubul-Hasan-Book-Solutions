100946dc2337a5ca7841b62177d13c03529a98b8
Sukarna Paul <sukarnapaul1893@gmail.com>
2019-04-12 18:00:47 +0600

    Add files via upload

---
+++ UVALive - 6821 Automated Checking Machine.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a,b,c,d,e;
+    int A,B,C,D,E;
+
+    while(cin>>a>>b>>c>>d>>e){
+        cin>>A>>B>>C>>D>>E;
+        if(a==A){
+            cout<<"N\n";
+            continue;
+        }
+        else if(b==B){
+            cout<<"N\n";
+            continue;
+        }
+        else if(c==C){
+            cout<<"N\n";
+            continue;
+        }
+        else if(d==D){
+            cout<<"N\n";
+            continue;
+        }
+        else if(e==E){
+            cout<<"N\n";
+            continue;
+        }
+        cout<<"Y\n";
+    }
+}
